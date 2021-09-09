a6476a3cad45bf8a45f13143f4c2ee988f0eeefa
Obi Nnaeto <cnnaeto@weh5201-a2.andrew.cmu.edu>
2021-09-09 10:51:19 -0400

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Chukwumaobim Sonne Paul Nnaeto on 9/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
